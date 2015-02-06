fc06f4ee58ee5b96566ca1287e3297a3f5d731b8
JungWooJoong@Gachon University <dnwnd2di@gmail.com>
2015-02-06 18:08:58 +0900

    hhi girls

---
+++ typica_db.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `Student` (
+  `idStudent` int(11) NOT NULL,
+  `StudentName` varchar(45) NOT NULL,
+  `StudentBirthDate` date NOT NULL,
+  `StudentPW` varchar(45) NOT NULL,
+  `StudentEmai` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idStudent`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `Student_cabinet` (
+  `StudentCabinet_ID` int(11) NOT NULL,
+  `Student_ID` int(11) NOT NULL,
+  `Cabinet_ID` int(11) NOT NULL,
+  `Time` time NOT NULL,
+  PRIMARY KEY (`StudentCabinet_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `Student_Fee` (
+  `idStudent_Fee` int(11) NOT NULL,
+  `Student_ID` int(11) NOT NULL,
+  `Fee_TF` tinyint(1) NOT NULL,
+  PRIMARY KEY (`idStudent_Fee`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `TotalCabinet` (
+  `idTotalCabinet` int(11) NOT NULL,
+  `Cabinet_Area` varchar(3) NOT NULL,
+  `Cabinet_Number` varchar(45) NOT NULL,
+  PRIMARY KEY (`idTotalCabinet`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
