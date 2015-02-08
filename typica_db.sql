8cf0eae16c4fc4e4cc718765f6396fc31c91ec00
Seri Lee <seri19951005@gmail.com>
2015-02-08 18:12:00 +0900

    test

--- typica_db.sql
+++ typica_db.sql
@@ -2,7 +2,7 @@ CREATE TABLE `Student` (
   `idStudent` int(11) NOT NULL,
   `StudentName` varchar(45) NOT NULL,
   `StudentBirthDate` date NOT NULL,
-  `StudentPW` varchar(45) NOT NULL,
+  `StudentPW` varchar(255) NOT NULL,
   `StudentEmail` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idStudent`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -18,7 +18,7 @@ CREATE TABLE `Student_cabinet` (
 CREATE TABLE `Student_Fee` (
   `idStudent_Fee` int(11) NOT NULL,
   `Student_ID` int(11) NOT NULL,
-  `Fee_TF` tinyint(1) NOT NULL,
+  `Fee_TF` tinyint(1) NULL,
   PRIMARY KEY (`idStudent_Fee`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
